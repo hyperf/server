449cf3b1136e54bea58fba64f343bb4752530775
李铭昕 <715557344@qq.com>
2019-04-17 12:53:44 +0800

    Added base server component.

---
+++ src/Command/StartServer.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Server\Command;
+
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Framework\Annotation\Command;
+use Hyperf\Server\ServerFactory;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @Command
+ */
+class StartServer extends SymfonyCommand
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        parent::__construct('start');
+        $this->container = $container;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->checkEnv($output);
+
+        $factory = $this->container->get(ServerFactory::class);
+        $config = $this->container->get(ConfigInterface::class)->get('server');
+
+        $factory->configure($config);
+        $factory->start();
+    }
+
+    private function checkEnv(OutputInterface $output)
+    {
+        if (ini_get_all('swoole')['swoole.use_shortname']['local_value'] !== 'Off') {
+            $output->writeln('<error>ERROR</error> Swoole short name have to disable before start server, please set swoole.use_shortname = \'Off\' into your php.ini.');
+            exit(0);
+        }
+    }
+}
---
+++ src/ConfigProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Server;
+
+use Hyperf\Server\Command\StartServer;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+            ],
+            'commands' => [
+                StartServer::class,
+            ],
+            'scan' => [
+                'paths' => [
+                    __DIR__,
+                ],
+            ],
+            'publish' => [
+                [
+                    'id' => 'config',
+                    'description' => 'The config for async queue.',
+                    'source' => __DIR__ . '/../publish/server.php',
+                    'destination' => BASE_PATH . '/config/autoload/server.php',
+                ],
+            ],
+        ];
+    }
+}
